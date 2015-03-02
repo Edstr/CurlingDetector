bff823403211547a71839ba571c7d77317bbf565
andy.cheung <andy.cheung@he-arc.ch>
2015-03-02 14:39:11 +0100

    Optimisser avec des valeurs pour le transformation "Hough Circle"

--- stoneDetector/stoneDetector.py
+++ stoneDetector/stoneDetector.py
@@ -3,11 +3,34 @@ __author__ = 'andy.cheung'
 import cv2
 import numpy as np
 
-img = cv2.imread('../images/curling1.png',0)
-img = cv2.medianBlur(img,5)
-cimg = cv2.cvtColor(img,cv2.COLOR_GRAY2BGR)
+img = cv2.imread("../images/curling1 - Copie.png",0)
+#img = cv2.imread("../images/curling3.png",0)
+thresh1 = cv2.threshold(img,127,255,cv2.THRESH_BINARY)[1]
 
-circles = cv2.HoughCircles(img,cv2.HOUGH_GRADIENT,1,20,param1=50,param2=30,minRadius=0,maxRadius=0)
+kernel = np.ones((4,4),np.uint8)
+
+opening = cv2.morphologyEx(thresh1, cv2.MORPH_OPEN, kernel)
+dilate = cv2.morphologyEx(opening, cv2.MORPH_DILATE, kernel)
+
+cv2.namedWindow('img', cv2.WINDOW_NORMAL)
+cv2.imshow('img',img)
+#cv2.imshow('thresh1',thresh1)
+#cv2.imshow('opening',opening)
+#cv2.imshow('dst_rt',dilate)
+
+var = cv2.bitwise_xor(opening,dilate)
+
+cv2.namedWindow('contour', cv2.WINDOW_NORMAL)
+
+#cv2.imshow('contour',var)
+#cv2.waitKey(0)
+
+# img = cv2.imread('../images/curling1.png',0)
+# img = cv2.medianBlur(img,5)
+cimg = cv2.cvtColor(var,cv2.COLOR_GRAY2BGR)
+
+#circles = cv2.HoughCircles(var,cv2.HOUGH_GRADIENT,1,20,param1=50,param2=30,minRadius=10,maxRadius=0)
+circles = cv2.HoughCircles(var,cv2.HOUGH_GRADIENT,1,20,param1=50,param2=30,minRadius=16,maxRadius=50)
 
 circles = np.uint16(np.around(circles))
 for i in circles[0,:]:
@@ -17,8 +40,8 @@ for i in circles[0,:]:
     cv2.circle(cimg,(i[0],i[1]),2,(0,0,255),3)
 
 cv2.imshow('detected circles',cimg)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+# cv2.waitKey(0)
+# cv2.destroyAllWindows()
 
 # img = cv2.imread('../images/curling1.png')
 #
@@ -37,4 +60,4 @@ cv2.destroyAllWindows()
 # cv2.imshow("b",b)
 
 cv2.waitKey(0)
-
+cv2.destroyAllWindows()
