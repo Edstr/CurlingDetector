0d6dbda2a8174a919c7995f8cea57f0cdbf3a459
andy.cheung <andy.cheung@he-arc.ch>
2015-03-02 16:00:30 +0100

    test en utilisant les filtres de couleurs

--- stoneDetector/stoneDetector.py
+++ stoneDetector/stoneDetector.py
@@ -3,34 +3,44 @@ __author__ = 'andy.cheung'
 import cv2
 import numpy as np
 
-img = cv2.imread("../images/curling1 - Copie.png",0)
-#img = cv2.imread("../images/curling3.png",0)
-thresh1 = cv2.threshold(img,127,255,cv2.THRESH_BINARY)[1]
 
-kernel = np.ones((4,4),np.uint8)
+def CannyThreshold(lowThreshold):
+    detected_edges = cv2.GaussianBlur(img,(3,3),0)
+    detected_edges = cv2.Canny(detected_edges,lowThreshold,lowThreshold*ratio,apertureSize = kernel_size)
+    dst = cv2.bitwise_and(img,img,mask = detected_edges) # just add some colours to edges from original image.
+    cv2.imshow('canny demo',dst)
 
-opening = cv2.morphologyEx(thresh1, cv2.MORPH_OPEN, kernel)
-dilate = cv2.morphologyEx(opening, cv2.MORPH_DILATE, kernel)
+lowThreshold = 0
+max_lowThreshold = 100
+ratio = 3
+kernel_size = 3
 
-cv2.namedWindow('img', cv2.WINDOW_NORMAL)
-cv2.imshow('img',img)
-#cv2.imshow('thresh1',thresh1)
-#cv2.imshow('opening',opening)
-#cv2.imshow('dst_rt',dilate)
 
-var = cv2.bitwise_xor(opening,dilate)
 
-cv2.namedWindow('contour', cv2.WINDOW_NORMAL)
+img = cv2.imread('../images/curling1 - Copie.png')
+blue,green,red = cv2.split(img)
 
-#cv2.imshow('contour',var)
-#cv2.waitKey(0)
+img = cv2.imread('../images/curling1 - Copie.png',0)
+img = cv2.medianBlur(img,5)
 
-# img = cv2.imread('../images/curling1.png',0)
-# img = cv2.medianBlur(img,5)
-cimg = cv2.cvtColor(var,cv2.COLOR_GRAY2BGR)
+cimg = cv2.cvtColor(img,cv2.COLOR_GRAY2BGR)
 
-#circles = cv2.HoughCircles(var,cv2.HOUGH_GRADIENT,1,20,param1=50,param2=30,minRadius=10,maxRadius=0)
-circles = cv2.HoughCircles(var,cv2.HOUGH_GRADIENT,1,20,param1=50,param2=30,minRadius=16,maxRadius=50)
+# define range of blue color in HSV
+upper_blue = np.array([20, 100, 100])
+lower_blue = np.array([30, 255, 255])
+
+# lower_blue = np.array([110,50,50])
+# upper_blue = np.array([130,255,255])
+
+# Threshold the HSV image to get only blue colors
+mask = cv2.inRange(cimg, lower_blue, upper_blue)
+
+# Bitwise-AND mask and original image
+res = cv2.bitwise_and(img,img, mask= mask)
+
+#circles = cv2.HoughCircles(img,cv2.HOUGH_GRADIENT,1,20,param1=50,param2=30,minRadius=10,maxRadius=0)
+circles = cv2.HoughCircles(mask,cv2.HOUGH_GRADIENT,1,20,param1=50,param2=30,minRadius=16,maxRadius=50)
+
 
 circles = np.uint16(np.around(circles))
 for i in circles[0,:]:
@@ -39,7 +49,22 @@ for i in circles[0,:]:
     # draw the center of the circle
     cv2.circle(cimg,(i[0],i[1]),2,(0,0,255),3)
 
+
 cv2.imshow('detected circles',cimg)
+
+cv2.imshow('frame',img)
+cv2.imshow('mask',mask)
+cv2.imshow('res',res)
+
+#cv2.namedWindow('canny demo')
+#cv2.createTrackbar('Min threshold','canny demo',lowThreshold, max_lowThreshold, CannyThreshold)
+#CannyThreshold(0) # initialization
+
+
+laplacian = cv2.Laplacian(cimg,cv2.CV_64F)
+sobelx = cv2.Sobel(cimg,cv2.CV_64F,1,0,ksize=5)
+sobely = cv2.Sobel(cimg,cv2.CV_64F,0,1,ksize=5)
+
 # cv2.waitKey(0)
 # cv2.destroyAllWindows()
 
@@ -58,6 +83,8 @@ cv2.imshow('detected circles',cimg)
 # #cv2.imshow("r",r)
 # #cv2.imshow("g",g)
 # cv2.imshow("b",b)
+
+print("Il y a %s cercles" %(len(circles)))
 
 cv2.waitKey(0)
 cv2.destroyAllWindows()
